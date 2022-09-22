9f2491b5b9d2f868972249fbe7d2c481e9187f57
damarus2022 <oscar.nyona@gmail.com>
2022-09-22 12:00:43 +0200

    if not found file .json

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ async function run() {
     const patterns = ['**/package.json', '!node_modules/**/package.json'];
     const globber = await glob.create(patterns.join('\n'));
     const files = await globber.glob();
+
+    var analysedPackageJsonCount = files.length;
+    var missingPackageLockJsonCount = 0;
     files.forEach(file => {
       try {
         fs.accessSync(path.join(path.dirname(file), 'package-lock.json'));
@@ -16,8 +19,21 @@ async function run() {
           title: "Missing package-lock.json",
           file: file
         });
+        missingPackageLockJsonCount++;
       }
     });
+
+    var analysis = core.summary.addHeading('Package.json analysis');
+    if (analysedPackageJsonCount > 0) {
+      if (missingPackageLockJsonCount > 0) {
+        analysis.addRaw(`Missing ${missingPackageLockJsonCount} package-lock.json files based on ${analysedPackageJsonCount} package.json files analysed`, true);
+      } else {
+        analysis.addRaw(`No missing package-lock.json files based on ${analysedPackageJsonCount} package.json files analysed`, true);
+      }
+    } else {
+      analysis.addRaw('No package.json found', true);
+    }
+    await analysis.write();
   }
   catch (error) {
     core.setFailed(error.message);
